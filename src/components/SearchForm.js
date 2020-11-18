77295bbfc9664c9b2ff71fa5d4cb8cced06b9f5a
Sean Moore <seanmoore556@gmail.com>
2020-11-18 12:02:11 -0500

    Add files via upload

---
+++ src/components/SearchForm.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useGlobalContext } from "../context";
+
+const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <section className="section search">
+      <form className="search_form" onSubmit={handleSubmit}>
+        <div className="form_control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default SearchForm;
---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about_section">
+      <h1 className="section_title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+        laudantium nam, similique quisquam laboriosam vero libero quia molestiae
+        consequatur quos corporis! Voluptates, hic! Eius reiciendis impedit,
+        modi doloribus fugiat ipsam alias esse voluptas error aliquam officiis
+        obcaecati cumque quo corporis?
+      </p>
+    </section>
+  );
+};
+
+export default About;
